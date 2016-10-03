4afbc04999b9dc131f27de2d73db6643378bbbe1
Zach Buchenau <ZBuchenau@yahoo.com>
2016-10-03 12:36:42 -0600

    added in the dynamic provider totals list

--- app/js/controllers/printController.js
+++ app/js/controllers/printController.js
@@ -4,14 +4,36 @@ function printController($scope, $http, server, localStorageService, $q, $locati
   var vm = this;
   console.log("PRINT CONTROLLER");
 
+  function sortObject(obj) {
+    var arr = [];
+    var prop;
+    for (prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        arr.push({
+          'key': prop,
+          'value': obj[prop]
+        });
+      }
+    }
+    arr.sort(function(a, b) {
+      return a.value - b.value;
+    });
+    return arr; // returns array
+  }
+
 
   vm.mediaPlan = mediaPlanService.mediaPlan;
   vm.info = mediaPlanService.mediaPlanInfo;
   vm.providerSpends = mediaPlanService.providerObj;
   // console.log(vm.mediaPlan);
-  // console.log(vm.info);
+  console.log(vm.info);
   console.log(vm.providerSpends);
 
 
+
+
+  var list = vm.providerSpends;
+  vm.providerArr = sortObject(list);
+  console.log(vm.providerArr);
 
 }
