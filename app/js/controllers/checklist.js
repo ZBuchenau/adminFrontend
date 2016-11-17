02bd6ff36a76962e9484e56fc7815fb4cb499cde
Zach Buchenau <ZBuchenau@yahoo.com>
2016-11-17 13:51:40 -0700

    added the functionality for the comments section on the checklist page

--- app/js/controllers/checklist.js
+++ app/js/controllers/checklist.js
@@ -21,23 +21,44 @@ function checkController($scope, $http, server, localStorageService, $q, $locati
     });
 
   vm.selectItemChanged = function(){
-    vm.mediaPlan = [];
-    vm.showHeaders = true;
-    console.log(vm.selectedItem);
-    mediaPlanService.getItems(server + '/users/mediaPlans/allTactics', {
-      mediaPlanId: vm.selectedItem
-    }).then(function(response){
-      console.log(response);
-      var info = response.data;
-      for(var i = 0; i < info.length; i++){
-        if(info[i].length > 0){
-          for(var j = 0; j < info[i].length; j++){
-            vm.mediaPlan.push(info[i][j]);
+    if(vm.selectedItem){
+      vm.mediaPlan = [];
+      // vm.showHeaders = true;
+      console.log("VM.SELECTEDITEM", vm.selectedItem);
+      mediaPlanService.getItems(server + '/users/mediaPlans/allTactics', {
+        mediaPlanId : vm.selectedItem
+      }).then(function(response){
+        console.log(response);
+        var info = response.data;
+        for(var i = 0; i < info.length; i++){
+          if(info[i].length > 0){
+            for(var j = 0; j < info[i].length; j++){
+              // console.log('CHECKING FOR COMMENTS', info[i][j]);
+              vm.mediaPlan.push(info[i][j]);
+            }
           }
         }
-      }
-      console.log(vm.mediaPlan);
-    });
+        if (vm.mediaPlan.length === 0){
+          vm.showHeaders = false;
+        } else {
+          vm.showHeaders = true;
+        }
+        console.log(vm.mediaPlan);
+      })
+      .then(function(){
+        mediaPlanService.getItems(server + '/users/tactics/pullcomments', {
+          'media_plan_id' : vm.selectedItem
+        }).then(function(response){
+          console.log(response);
+          vm.comments.checklist_comments = response.data;
+        });
+      });
+    } else {
+      vm.showHeaders = false;
+      vm.mediaPlan = [];
+      vm.comments.checklist_comments = '';
+      console.log(vm.comments.checklist_comments);
+    }
   };
 
 
@@ -52,13 +73,12 @@ function checkController($scope, $http, server, localStorageService, $q, $locati
   };
 
   vm.commentSubmit = function(item){
-    // console.log(vm.selectedItem);
     vm.comments.media_plan_id = parseInt(vm.selectedItem);
-    console.log(vm.comments);
-    // mediaPlanService.tacticSubmit(server + '/users/tactics/checkedit', item)
-    // .then(function(response){
-    //   console.log(response);
-    // });
+    console.log(item);
+    mediaPlanService.tacticSubmit(server + '/users/tactics/checkcomments', item)
+    .then(function(response){
+      console.log(response);
+    });
   };
 
 
