3c4cacc89ade84058a4bd7fb42d4ce7b244d5257
Zach Buchenau <ZBuchenau@yahoo.com>
2016-12-22 17:32:49 -0700

    working on inserting reports into the database

--- app/js/controllers/reportController.js
+++ app/js/controllers/reportController.js
@@ -3,10 +3,25 @@ app.controller('reportController', ['$scope', '$http', 'server', 'localStorageSe
 function reportController($scope, $http, server, localStorageService, $q, $location, authService, mediaPlanService) {
   var vm = this;
 
+  vm.newReport = {};
 
   mediaPlanService.pullMedia(server + '/clients')
     .then(function(response){
       vm.clientList = response.data;
   });
+
+  vm.createReport = function(item){
+    console.log(item);
+    mediaPlanService.tacticSubmit(server + '/reports', item)
+      .then(function(response){
+        console.log(response);
+        if(response !== false){
+          console.log('success');
+          vm.newReport = {};
+        } else {
+          alert('CHECK CLIENT FORM FOR ERRORS...');
+        }
+      });
+  };
 
 }
